52bcbfc5490f2786d1c5ccfebb2876621f532448
leegusrb <jeffsep24@gmail.com>
2025-07-26 20:24:24 +0900

    [Feat] 데이터베이스 연결 설정 변경

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ engine = create_engine(
 	connect_args={"check_same_thread": False},
 	pool_size=20,
 	max_overflow=30,
-	poll_pre_ping=True,
+	pool_pre_ping=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
